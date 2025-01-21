7353c21abff937ab1cca1cf6a6837dfc9309ecfe
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-01-22 03:10:36 +0500

    chore: company added

--- purchase_requisition/purchase_requisition/doctype/patient_appointment_entry/patient_appointment_entry.py
+++ purchase_requisition/purchase_requisition/doctype/patient_appointment_entry/patient_appointment_entry.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PatientAppointmentEntry(Document):
-	pass
+	def validate(self):
+		self.get_user_default_company()
+		
+	def get_user_default_company(self):
+		default_company = frappe.defaults.get_user_default("Company")
+		if default_company:
+			self.company = default_company 
