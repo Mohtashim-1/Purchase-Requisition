731e66ad21b9fc29411c8d6de23576ee379c0ca6
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-05 10:02:03 +0500

    chore: route from po to purchase invoice

--- purchase_requisition/purchase_requisition/public/js/purchase_order.js
+++ purchase_requisition/purchase_requisition/public/js/purchase_order.js
@@ -51,3 +51,47 @@ frappe.ui.form.on('Purchase Order', {
         }
     }
 });
+
+
+
+  
+frappe.ui.form.on('Purchase Order', {
+    refresh: function (frm) {
+        frm.add_custom_button(__('Purchase Invoice New'), function () {
+            // Create a new Purchase Invoice and set values
+            frappe.model.with_doctype('Purchase Invoice', function () {
+                let pi = frappe.model.get_new_doc('Purchase Invoice');
+
+                // Set values from Purchase Order
+                pi.supplier = frm.doc.supplier;
+                // pi.purchase_order = frm.doc.name;
+
+                // Optionally copy items from the Purchase Order
+                frm.doc.items.forEach(item => {
+                    let pi_item = frappe.model.add_child(pi, 'items');
+                    pi_item.item_code = item.item_code;
+                    pi_item.item_name = item.item_name;
+                    pi_item.qty = item.qty;
+                    pi_item.uom = item.uom;
+                    pi_item.rate = item.rate;
+                    pi_item.discount_percentage = item.discount_percentage;
+                    pi_item.discount_amount = item.discount_amount;
+
+                    pi_item.custom_material_request = item.material_request;
+                    pi_item.custom_material_request_item = item.material_request_item;
+
+                    pi_item.custom_purchase_requisition = item.custom_purchase_requisition;
+                    pi_item.custom_purchase_requisition_item = item.custom_purchase_requisition_item;
+
+                    pi_item.po_detail = item.name;
+                    pi_item.purchase_order = frm.doc.name;
+
+
+                });
+
+                // Route to the new Purchase Invoice form
+                frappe.set_route('Form', 'Purchase Invoice', pi.name);
+            });
+        }, __("Create"));
+    }
+});
