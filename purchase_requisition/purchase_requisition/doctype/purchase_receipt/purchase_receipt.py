3a0cb7cbf88aba746000a4351e7ba82a185ffcba
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-19 00:30:15 +0500

    chore: pr ref added to grn

--- purchase_requisition/purchase_requisition/doctype/purchase_receipt/purchase_receipt.py
+++ purchase_requisition/purchase_requisition/doctype/purchase_receipt/purchase_receipt.py
@@ -2,4 +2,14 @@ import frappe
 from frappe.model.document import Document
 
 def get_pr_in_grn(doc, method):
-    pass
+    for i in doc.items:
+        purchase_order_ref = frappe.get_doc("Purchase Order Item",i.purchase_order_item)
+        
+        purchase_requisition = purchase_order_ref.custom_purchase_requisition
+        purchase_requisition_item = purchase_order_ref.custom_purchase_requisition_item
+        
+        i.custom_purchase_requisition = purchase_requisition
+        i.custom_purchase_requisition_item = purchase_requisition_item
+        
+        frappe.db.commit()
+    
